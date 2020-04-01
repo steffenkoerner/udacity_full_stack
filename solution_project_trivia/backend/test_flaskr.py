d7becc18d6f80850f2b0f4c5c2f626e953b7ed69
steffen <kornstef90@googlemail.com>
2020-04-01 18:21:50 +0200

    Apply formattin

--- solution_project_trivia/backend/test_flaskr.py
+++ solution_project_trivia/backend/test_flaskr.py
@@ -25,9 +25,12 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
+        pass
+
+    def test_first(self):
         pass
 
     """
