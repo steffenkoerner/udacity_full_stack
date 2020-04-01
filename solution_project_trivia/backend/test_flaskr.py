cc969a76aa98544fd74d2eb84ddf4659315d6a1f
steffen <kornstef90@googlemail.com>
2020-04-01 18:20:23 +0200

    connect to database

--- solution_project_trivia/backend/test_flaskr.py
+++ solution_project_trivia/backend/test_flaskr.py
@@ -14,8 +14,9 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = "trivia"
+        self.database_path = "postgresql://postgres:postgres@{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
