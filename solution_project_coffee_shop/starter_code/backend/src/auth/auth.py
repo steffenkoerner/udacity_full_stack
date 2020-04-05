661835d5766c86e9fa1c8a98aa4506ffaca2c010
steffen <kornstef90@googlemail.com>
2020-04-05 20:49:58 +0200

    Remove comments

--- solution_project_coffee_shop/starter_code/backend/src/auth/auth.py
+++ solution_project_coffee_shop/starter_code/backend/src/auth/auth.py
@@ -4,7 +4,6 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-# TODO: Steffen adapt this credentials
 AUTH0_DOMAIN = 'blub.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee'
